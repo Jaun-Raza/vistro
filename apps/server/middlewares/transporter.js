2ab3a0c0cd74d514222a28c050d4648fef75aa47
Jaun-Raza <jaundev768@gmail.com>
2025-06-27 19:56:38 +0500

    Admin Prod Fixes

--- apps/server/middlewares/transporter.js
+++ apps/server/middlewares/transporter.js
@@ -2,16 +2,16 @@ import nodemailer from 'nodemailer';
 import 'dotenv/config';
 
 const emailConfig = {
-  host: 'smtp.zoho.com',
+  host: 'smtp.zoho.eu',
   port: 465,
   secure: true,
   auth: {
     user: process.env.USER,
     pass: process.env.PASS
   },
-  tls: {
-    rejectUnauthorized: false
-  }
+  // tls: {
+  //   rejectUnauthorized: false
+  // }
 };
 
 const transporter = nodemailer.createTransport(emailConfig);
